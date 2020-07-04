9d1ed7976cc5d9531cfe2558d8a319cfd311fa02
dmitry <dmitrykataev4@gmail.com>
2020-07-04 20:54:01 +0300

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -3,7 +3,7 @@ document.getElementById('flang').onclick = function(){
 			let password = document.getElementById('password').value;
 
 			if (login == 'dmitry' && password == '34')
-				document.location.replace("");
+				document.location.replace("https://digma1402.github.io/load.ru/");
 			else
 				alert('Ошибка');
 		}
